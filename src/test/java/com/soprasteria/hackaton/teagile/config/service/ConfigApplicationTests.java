da756cde144c21bdca31a648c3f909d21c3adb2c
idosinchuk <idosinchuk@ITEM-S84804.emea.msad.sopra>
2019-10-01 09:42:57 +0200

    Adding classes

--- src/test/java/com/soprasteria/hackaton/teagile/config/service/ConfigApplicationTests.java
+++ src/test/java/com/soprasteria/hackaton/teagile/config/service/ConfigApplicationTests.java
@@ -1,4 +1,4 @@
-package com.soprasteria.hackaton.teagile.zuul.service;
+package com.soprasteria.hackaton.teagile.config.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ZuulGatewayServerApplicationTests {
+public class ConfigApplicationTests {
 
 	@Test
 	public void contextLoads() {
